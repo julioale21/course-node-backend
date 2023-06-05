06f6569c10c2dbc9de0d5b98ab0cb5a56bf0a7d1
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-06-05 17:19:36 -0300

    created user model

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    image: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    },
+});
+
+module.exports = model('User', userSchema);
